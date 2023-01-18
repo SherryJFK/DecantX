095e0e8842ec3ad409cac99141b7005bed9fa1e4
Sherryluanq <79776513+Sherryluanq@users.noreply.github.com>
2023-01-18 18:42:53 -0500

    Initial commit of Shella theme

---
+++ assets/custom.js
@@ -0,0 +1,17 @@
+(function($){
+ 
+
+  
+  
+
+$( ".mw-gp__table__cell--product a" ).attr('href', '').css({'cursor': 'pointer', 'pointer-events' : 'none'});
+
+  
+  
+  
+  
+
+                     
+  
+    /*END CUSTOM JS*/
+})(jQueryTheme);
